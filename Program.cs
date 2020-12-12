11ccf062a3d39c7a52582cca4428268143c5d56a
Lova Widmark <znurree@gmail.com>
2020-12-12 16:20:17 +0100

    Foo

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Antlr4.Runtime;
 using Antlr4.Runtime.Misc;
+using LLVMSharp.Interop;
 
 namespace dz
 {
@@ -14,17 +14,19 @@ namespace dz
 
     class DzFunctionCall : DzValue
     {
-		private readonly string name;
         private readonly DzValue parent;
         private readonly List<DzValue> arguments;
+
+		public string Name { get; }
 
         public DzFunctionCall(string name
 			, DzValue parent
 			)
         {
-			this.name = name;
             this.parent = parent;
 			this.arguments = new List<DzValue>();
+
+			this.Name = name;
         }
 
 		public void AddArgument(DzValue argument)
@@ -39,7 +41,7 @@ namespace dz
 				.Where(x => x != null)
 				.Select(x => x.ToString());
 
-			return $"{this.name}({string.Join(", ", arguments)})";
+			return $"{this.Name}({string.Join(", ", arguments)})";
 		}
     }
 
@@ -122,35 +124,54 @@ namespace dz
 
 	class DzReturn : DzValue
 	{
-		private readonly DzValue value;
-
-		public DzReturn(DzValue value)
-		{
-			this.value = value;
-		}
+		public DzValue Value { get; set; }
 
         public override string ToString()
         {
-            return $"return {this.value};";
+            return $"return {this.Value}";
         }
     }
 
+	class DzTerminator : DzValue
+	{
+		public DzValue Value { get; set; }
+
+        public override string ToString()
+        {
+            return this.Value.ToString();
+        }
+    
+	}
+
     class Visitor : dzBaseVisitor<(DzValue, DzValue)>
     {
+		private readonly bool isDependency;
         private readonly DzValue parent;
 		private readonly DzValue deepest;
+		private readonly LLVMModuleRef module;
 
-        public Visitor(DzValue parent, DzValue deepest)
+        public Visitor(bool isDependency
+			, DzValue parent
+			, DzValue deepest
+			, LLVMModuleRef module
+			)
         {
+			this.isDependency = isDependency;
             this.parent = parent;
 			this.deepest = deepest;
+			this.module = module;
         }
 
-        private static (IEnumerable<DzValue>, DzValue) Fold((IEnumerable<DzValue> Arguments, DzValue Consumer) accumulator
+        private (IEnumerable<DzValue>, DzValue) Fold((IEnumerable<DzValue> Arguments, DzValue Consumer) accumulator
             , dzParser.ExpressionContext expression
             )
         {
-			var visitor = new Visitor(accumulator.Consumer, accumulator.Consumer);
+			var visitor = new Visitor(this.isDependency
+				, accumulator.Consumer
+				, accumulator.Consumer
+				, this.module
+				);
+				
             var (argument, function) = visitor.Visit(expression);
 
             return (accumulator.Arguments.Append(argument), function);
@@ -158,11 +179,16 @@ namespace dz
 
         public override (DzValue, DzValue) VisitCall(dzParser.CallContext context)
         {
-			var closure = new DzClosure(this.deepest);
+			var ret = new DzReturn
+			{
+				Value = this.deepest
+			};
+
+			var closure = new DzClosure(ret);
 			var call = new DzFunctionCall(context.ID().GetText(), closure);
 
 			var (arguments, producer) = context.expression()
-				.Aggregate((Enumerable.Empty<DzValue>(), (DzValue)call), Fold);
+				.Aggregate((Enumerable.Empty<DzValue>(), (DzValue)call), this.Fold);
 
 			foreach (var argument in arguments)
 			{
@@ -172,23 +198,102 @@ namespace dz
 			return (closure.Parameter, producer);
         }
 
+		class TypeHarvester : dzBaseVisitor<LLVMTypeRef>
+		{
+            public override LLVMTypeRef VisitTypeName([NotNull] dzParser.TypeNameContext context)
+            {
+				return context.ID().GetText() switch
+				{
+					"int" => LLVMTypeRef.Int32,
+					_ => throw new NotSupportedException()
+				};
+            }
+
+            protected override LLVMTypeRef AggregateResult(LLVMTypeRef aggregate, LLVMTypeRef nextResult)
+            {
+                return base.AggregateResult(aggregate, nextResult);
+            }
+		}
+
+		private IEnumerable<LLVMTypeRef> GetArgumentTypes(dzParser.FunctionContext context)
+		{
+			var typeHarvester = new TypeHarvester();
+
+			foreach (var argument in context.argument())
+			{
+				yield return typeHarvester.Visit(argument);
+			}
+
+			if (this.isDependency)
+			{
+				yield return LLVMTypeRef.CreateFunction(LLVMTypeRef.Int32, new[] { LLVMTypeRef.Int32 }, false);	
+			}
+		}
+
 		public override (DzValue, DzValue) VisitFunction(dzParser.FunctionContext context)
 		{
-			return this.Visit(context.block());
+			var name = context.ID().GetText();
+
+			var (value, f) = this.Visit(context.block());
+
+			var parameterTypes = this
+				.GetArgumentTypes(context)
+				.ToArray();
+
+			var functionType = LLVMTypeRef.CreateFunction(LLVMTypeRef.Int32, parameterTypes, false);
+			var prototype = module.AddFunction(name, functionType);
+
+			Console.Write($"int {name}(");
+
+			if (this.isDependency)
+			{
+				Console.Write("func<int, int> consumer");
+			}
+
+			Console.WriteLine(")");
+			Console.WriteLine("{");
+			Console.Write("\t");
+			Console.WriteLine(value);
+			Console.WriteLine("}");
+			Console.WriteLine();
+
+			return (value, f);
 		}
 
         public override (DzValue, DzValue) VisitRet([NotNull] dzParser.RetContext context)
         {
-			var call = new DzFunctionCall("consumer", null);
-			var visitor = new Visitor(call, call);
+			if (this.isDependency)
+			{
+				var call = new DzFunctionCall("consumer", null);
+				var visitor = new Visitor(this.isDependency, call, call, this.module);
 
-            var (value, f) = visitor.Visit(context.value);
+				var (value, f) = visitor.Visit(context.value);
 
-			call.AddArgument(value);
+				call.AddArgument(value);
 
-			var ret = new DzReturn(f);
+				var ret = new DzReturn
+				{
+					Value = f					
+				};
 
-			return (ret, f);
+				return (ret, f);
+			}
+			else
+			{
+				var terminator = new DzTerminator();
+				var visitor = new Visitor(this.isDependency, terminator, terminator, this.module);
+
+				var (value, f) = visitor.Visit(context.value);
+
+				terminator.Value = value;
+
+				var ret = new DzReturn
+				{
+					Value = f
+				};
+
+				return (ret, f);
+			}
         }
 
 		public override (DzValue, DzValue) VisitBlock(dzParser.BlockContext context)
@@ -198,13 +303,21 @@ namespace dz
 
 		public override (DzValue, DzValue) VisitProgram(dzParser.ProgramContext context)
 		{
+			var dependencyVisitor = new DependencyVisitor();
+
+			var dependencies = dependencyVisitor
+				.Visit(context)
+				.ToHashSet();
+
 			foreach (var function in context.function())
 			{
-				var (f, q) = this.Visit(function);
+				var name = function.ID().GetText();
 
-				Console.WriteLine($"{function.ID().GetText()}:");
-				Console.WriteLine(f);
-				Console.WriteLine(q);
+				var isDependency = dependencies.Contains(name);
+
+				var visitor = new Visitor(isDependency, null, null, this.module);
+
+				var (f, q) = visitor.Visit(function);
 			}
 
 			return this.DefaultResult;
@@ -214,10 +327,10 @@ namespace dz
 		{
 			var binary = new DzBinary();
 
-			var rightVisitor = new Visitor(binary, this.deepest);
+			var rightVisitor = new Visitor(this.isDependency, binary, this.deepest, this.module);
 			var (right, f) = rightVisitor.Visit(context.right);
 
-			var leftVisitor = new Visitor(f, f);
+			var leftVisitor = new Visitor(this.isDependency, f, f, this.module);
 			var (left, k) = leftVisitor.Visit(context.left);
 
 			binary.Left = left;
@@ -245,6 +358,8 @@ namespace dz
     {
         static void Main(string[] args)
         {
+			var module = LLVMModuleRef.CreateWithName("my cool jit");
+
             using (var stream = new FileStream("main.dz", FileMode.Open))
             {
                 var input = new AntlrInputStream(stream);
@@ -253,7 +368,7 @@ namespace dz
                 var tokens = new CommonTokenStream(lexer);
                 var parser = new dzParser(tokens);
 
-                var visitor = new Visitor(null, null);
+                var visitor = new Visitor(false, null, null, module);
 
 				var program = parser.program();
 
